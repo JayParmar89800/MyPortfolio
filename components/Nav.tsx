f885be0820bcfee6473d805f9f3b3b868a4274ef
JayParmar89800 <jayp89800@gmail.com>
2024-06-29 18:18:08 +0530

    Add wonderfull navbar and change the og images

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,18 +4,28 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { siteConfig } from "@/config/site";
+import { Button } from "./ui/button";
 
 const Nav = () => {
-    const pathname=usePathname();
+  const pathname = usePathname();
   return (
-    <nav className="flex gap-8">
+    <nav className="flex items-center gap-8">
       {siteConfig.navItems.map((link, index) => {
         return (
-          <Link href={link.path} key={index} className={`${link.path==pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path == pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}
+          >
             {link.label}
           </Link>
         );
       })}
+      <Link href="/contact">
+        <Button>Hire me</Button>
+      </Link>
     </nav>
   );
 };
