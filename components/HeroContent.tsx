4082ec2f532b64a74d0b9be30aed184799f4f70c
JayParmar89800 <jayp89800@gmail.com>
2024-06-30 11:24:47 +0530

    run time warning solve and Front-end developer button add animation updown infinite

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 import React from "react";
 import { FiDownload } from "react-icons/fi";
-
-// Components
 import Stats from "./Stats";
 import { Button } from "./ui/button";
 import Socials from "./Socials";
@@ -10,6 +10,7 @@ import Clouds from "./Clouds";
 import AnimatedGradientText from "./magicui/animated-gradient-text";
 import { ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 
 const HeroContent = () => {
   return (
@@ -18,24 +19,32 @@ const HeroContent = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between">
           {/* text */}
           <div className="text-center xl:text-left flex flex-col justify-center xl:justify-start">
-            <div className="xl:w-auto w-full flex justify-center xl:justify-start mb-3">
+            <motion.div
+              animate={{
+                y: [0, -10],
+              }}
+              transition={{
+                duration: 1,
+                repeat: Infinity,
+                repeatType: "mirror",
+              }}
+              className="xl:w-auto w-full flex justify-center xl:justify-start mb-3"
+            >
               <BoxReveal boxColor={"#005b94"} duration={0.5}>
-                {/* <span className="text-xl mx-auto">Frontend Developer</span> */}
-                {/* <div className="z-10 flex min-h-[16rem] items-center justify-center"> */}
-                  <AnimatedGradientText>
-                    🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
-                    <span
-                      className={cn(
-                        `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
-                      )}
-                    >
-                      Front-end Developer
-                    </span>
-                    <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
-                  </AnimatedGradientText>
-                {/* </div> */}
+              
+                <AnimatedGradientText>
+                  🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
+                  <span
+                    className={cn(
+                      `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
+                    )}
+                  >
+                    Front-end Developer
+                  </span>
+                  <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+                </AnimatedGradientText>
               </BoxReveal>
-            </div>
+            </motion.div>
 
             <h1 className="h1 mb-6">
               <div className="xl:w-auto w-full flex justify-center xl:justify-start">
